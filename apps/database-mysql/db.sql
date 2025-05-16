aa0bef139208a0cc96542d4e57aa05f1b70e7bb4
Itsakaseru <sakii@itsakaseru.me>
2025-05-17 02:55:04 +0700

    Create MySQL setup script according to ERD

---
+++ apps/database-mysql/db.sql
@@ -0,0 +1,126 @@
+## Create Schema
+CREATE DATABASE AuthDB;
+USE AuthDB;
+
+CREATE TABLE User
+(
+    id          INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    hash        VARCHAR(72) NOT NULL,
+    createdAt   TIMESTAMP NOT NULL,
+    updatedAt   TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+
+CREATE DATABASE EmployeeDB;
+USE EmployeeDB;
+
+CREATE TABLE Department
+(
+    id      INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    name    TEXT NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Position
+(
+    id      INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    name    TEXT NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Employee
+(
+    id              INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    userId          INTEGER NOT NULL UNIQUE,
+    name            TEXT NOT NULL,
+    gender          VARCHAR(1) NOT NULL,
+    dob             DATETIME NOT NULL,
+    departmentId    INTEGER NOT NULL,
+    positionId      INTEGER NOT NULL,
+    createdAt       TIMESTAMP NOT NULL,
+    updatedAt       TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (departmentId) REFERENCES Department (id)
+);
+
+CREATE TABLE EmployeeHistory
+(
+    id              INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    userId          INTEGER NOT NULL UNIQUE,
+    name            TEXT NOT NULL,
+    gender          VARCHAR(1) NOT NULL,
+    dob             DATETIME NOT NULL,
+    departmentId    INTEGER NOT NULL,
+    positionId      INTEGER NOT NULL,
+    createdAt       TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (departmentId) REFERENCES Department (id)
+);
+
+CREATE DATABASE AttendanceDB;
+USE AttendanceDB;
+
+CREATE TABLE AttendanceType
+(
+    id   INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    name TEXT    NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Attendance
+(
+    id          INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    userId      INTEGER NOT NULL,
+    typeId      INTEGER NOT NULL,
+    dateTime    TIME,
+    photo       TEXT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (typeId) REFERENCES AttendanceType (id)
+);
+
+CREATE TABLE TargetAttendance
+(
+    id          INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    userId      INTEGER NOT NULL,
+    weekday     INTEGER NOT NULL,
+    startTime   TIME NOT NULL,
+    endTime     TIME NOT NULL,
+    createdAt   TIMESTAMP NOT NULL,
+    updatedAt   TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE TargetAttendanceHistory
+(
+    id          INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+    userId      INTEGER NOT NULL,
+    weekday     INTEGER NOT NULL,
+    startTime   TIME NOT NULL,
+    endTime     TIME NOT NULL,
+    createdAt   TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+## Create User Auth
+CREATE USER 'auth'@'%' IDENTIFIED BY 'authPassword';
+CREATE USER 'employee'@'%' IDENTIFIED BY 'employeePassword';
+CREATE USER 'attendance'@'%' IDENTIFIED BY 'attendancePassword';
+
+
+## Grant Permission
+GRANT ALL ON AuthDB.* TO 'auth'@'%';
+GRANT ALL ON EmployeeDB.* TO 'employee'@'%';
+GRANT ALL ON AttendanceDB.* TO 'attendance'@'%';
+
+FLUSH PRIVILEGES;
