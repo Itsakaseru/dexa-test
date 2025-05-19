e4af7d627b720c858cbc5fe168b24872a11d8885
Itsakaseru <sakii@itsakaseru.me>
2025-05-19 20:39:31 +0700

    Refactor(web-attendance): Create footer component

---
+++ apps/web-attendance/app/components/ui/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-col items-center text-center w-full p-6 text-sm gap-2">
+      <p className="text-xs">Server Time: DDDD MM YYYY - HH:MM:SS</p>
+      <footer>Copyright (c) 2025 Lemuel Lancaster. All rights reserved.</footer>
+    </div>
+  );
+}
--- apps/web-attendance/app/home/index.tsx
+++ apps/web-attendance/app/home/index.tsx
@@ -1,8 +1,9 @@
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
+import Footer from "~/components/ui/footer";
 import { Form } from "react-router";
 
-export default function Home() {
+export default function Index() {
   return (
     <main className="flex flex-col w-screen h-screen items-center justify-center">
       <div className="flex w-screen grow items-center justify-center gap-32">
@@ -12,16 +13,15 @@ export default function Home() {
             Attendance Portal
           </p>
         </div>
-        <div className="w-[1px] h-1/4 rounded-full bg-gray-200"/>
-        <Form method="post" action="/" className="flex flex-col w-full max-w-2/12 gap-4">
-          <Input name="email" type="email" placeholder="Email" />
-          <Input name="password" type="password" placeholder="Password" />
-          <Button className="cursor-pointer">Login</Button>
-        </Form>
+        <div className="bg-white rounded-2xl w-2/12 px-8 py-8">
+          <Form method="post" action="/" className="flex flex-col gap-4">
+            <Input name="email" type="email" placeholder="Email" />
+            <Input name="password" type="password" placeholder="Password" />
+            <Button className="cursor-pointer mt-2">Login</Button>
+          </Form>
+        </div>
       </div>
-      <div className="flex justify-center w-full border-t-1 border-gray-200 p-3 text-sm">
-        Copyright (c) 2025 Lemuel Lancaster. All rights reserved.
-      </div>
+      <Footer />
     </main>
   );
 }
