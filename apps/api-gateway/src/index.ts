acb755ad6a617723c108955a60b46e22ab60734a
Itsakaseru <sakii@itsakaseru.me>
2025-05-21 15:19:45 +0700

    Feat(api-gateway): Implement express.js security best practices

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -10,6 +10,23 @@ import {verifyAccessToken} from "./middlewares/verify.middleware";
 import attendanceRoute from "./routes/attendance.route";
 import employeeRoute from "./routes/employee.route";
 import httpProxy from "http-proxy";
+import helmet from "helmet";
+
+import { rateLimit } from 'express-rate-limit'
+
+export const rateLimitAll = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	limit: 10000,            // 10000 request per windowMs
+	standardHeaders: "draft-8", 
+	legacyHeaders: false, 
+});
+
+export const rateLimitAuth = rateLimit({
+	windowMs: 1 * 60 * 1000,  // 1 minutes
+	limit: 100,               // 100 request per windowMs
+	standardHeaders: "draft-8", 
+	legacyHeaders: false, 
+});
 
 dotenv.config();
 
@@ -20,6 +37,7 @@ const logger = pinoHttp();
 const prisma = new PrismaClient();
 const apiProxy = httpProxy.createProxyServer();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -28,14 +46,15 @@ app.use(cors({
   origin: true
 }));
 app.use(logger);
+app.use(helmet());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", rateLimitAll, (req: Request, res: Response) => {
   res.json({
     time: new Date().toUTCString(),
   });
 });
 
-app.get("/uploads/*any", verifyAccessToken, (req: Request, res: Response) => {
+app.get("/uploads/*any", rateLimitAll, verifyAccessToken, (req: Request, res: Response) => {
   req.url = req.url.replace("/uploads", "");
 
   apiProxy.web(req, res, { target: "http://localhost:3002" }, (err: Error) => {
@@ -44,11 +63,11 @@ app.get("/uploads/*any", verifyAccessToken, (req: Request, res: Response) => {
   });
 });
 
-app.use("/auth", authRoute);
-app.use("/user", verifyAccessToken, userRoute);
+app.use("/auth", rateLimitAuth, authRoute);
+app.use("/user", rateLimitAll, verifyAccessToken, userRoute);
 
-app.use("/attendance", verifyAccessToken, attendanceRoute);
-app.use("/employee", verifyAccessToken, employeeRoute);
+app.use("/attendance", rateLimitAll, verifyAccessToken, attendanceRoute);
+app.use("/employee", rateLimitAll, verifyAccessToken, employeeRoute);
 
 app.listen(PORT, async () => {
   await prisma.$connect();
